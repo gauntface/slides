c7a9124e5b2362bc7e6fec909b5cc6b8bc166c66
Matt Gaunt <matt@gauntface.co.uk>
2016-05-23 15:11:21 +0100

    Making slide scale down to fit the available space

--- src/scripts/app-controller.js
+++ src/scripts/app-controller.js
@@ -1,14 +1,29 @@
+const SLIDE_DIMENSIONS = {
+  width: 1080,
+  height: 1080 * (9 / 16)
+}
+
 class AppController {
   constructor() {
     this.numberSlides();
+    this.fitSlideToWindow();
   }
 
   numberSlides() {
     const slides = document.querySelectorAll('gf-slide');
     slides[0].isVisible = true;
-    
+
     slides.forEach((slide, index) => {
       slide.pageNumber = index;
+    });
+  }
+
+  fitSlideToWindow() {
+    const scaleFactor = document.body.clientWidth / SLIDE_DIMENSIONS.width;
+    const slides = document.querySelectorAll('gf-slide');
+    slides.forEach((slide, index) => {
+      slide.style.transformOrigin = '0 0';
+      slide.style.transform = `scale(${scaleFactor})`;
     });
   }
 }
